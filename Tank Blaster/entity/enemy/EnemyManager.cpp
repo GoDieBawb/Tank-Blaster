1c819fad713185eb6641d13280b8e42e0cc4bd9a
Altermia <jjkim913@gmail.com>
2017-03-18 16:59:52 -0700

    refilling lanes for enemies

--- Tank Blaster/entity/enemy/EnemyManager.cpp
+++ Tank Blaster/entity/enemy/EnemyManager.cpp
@@ -5,6 +5,7 @@ struct Enemy {
 	Node*	  model;
 	Enemy(Node& n, Behavior& b);
 	int health;
+	int lane;
 };
 
 Enemy::Enemy(Node& n, Behavior& b) {
@@ -26,7 +27,7 @@ class EnemyManager {
 		Node enemyNode;
 		EnemyManager();
 		Enemy* enemies[12];	
-
+		bool lanes[10];
 };
 
 //Constructor initializes data memebrs
@@ -39,50 +40,53 @@ EnemyManager::EnemyManager() {
 
 void EnemyManager::move() {
 
-	
-	//Create an enemy if not enough
-	if (enemyCount < 10) {
+	for (int j = 0; j < 10; j++) {
+		//Create an enemy if not enough
+		if (enemyCount < 10) {
 
-		//Randomize location
-		int x = 0 + (rand() % (int) (800 - 0 + 1));
-		//int y = 0 + (rand() % (int) (500 - 0 + 1));
-
-		
-		//Local Variable put on heap
-		Tank* 				  t = new Tank(Vec(x,0,0));
-		StreetAttackBehavior* b = new StreetAttackBehavior();
-		Enemy* 				  e = new Enemy(*t,*b);
-
-		//Add to list and render node
-		enemies[enemyCount] = e;
-		enemyNode.attachChild(*t);
-		enemyCount++;
-
-	}
+			if (lanes[j] == false) {
+				//Randomize location
+				int x = 800 - ((j * 70) - 20);
+				//int y = 0 + (rand() % (int) (500 - 0 + 1));
 
 
-	//Act on list of enemies
-	for (int i = 0; i < enemyCount; i++) {
-
-		enemies[i]->behavior->behave(*enemies[i]->model);
-		
-		if (enemies[i]->model->location.y > 600 || enemies[i]->health <= 0) {
-
-			//Detach Child From Render Node
-			enemyNode.detachChild(*enemies[i]->model);
-			//Delete because tanks put on heap
-			delete enemies[i]->model;
-			delete enemies[i]->behavior;
-			delete enemies[i];
-			//Remove Tank from List
-			enemies[i] = enemies[enemyCount-1];
-			enemyCount--;
-
+				//Local Variable put on heap
+				Tank* 				  t = new Tank(Vec(x,0,0));
+				StreetAttackBehavior* b = new StreetAttackBehavior();
+				Enemy* 				  e = new Enemy(*t,*b);
+				e->lane = j;
+				//Add to list and render node
+				enemies[enemyCount] = e;
+				enemyNode.attachChild(*t);
+				enemyCount++;
+				lanes[j] = true;
+			}
 		}
 
-	}
 
+		//Act on list of enemies
+		for (int i = 0; i < enemyCount; i++) {
+
+			enemies[i]->behavior->behave(*enemies[i]->model);
+
+			if (enemies[i]->model->location.y > 600 || enemies[i]->health <= 0) {
+
+				//Detach Child From Render Node
+				enemyNode.detachChild(*enemies[i]->model);
+				//Delete because tanks put on heap
+				lanes[enemies[i]->lane] = false;
+				delete enemies[i]->model;
+				delete enemies[i]->behavior;
+				delete enemies[i];
+				//Remove Tank from List
+				enemies[i] = enemies[enemyCount-1];
+				enemyCount--;
+			}
+
+		}
+	}
 }
+
 
 //Called on Loop
 void EnemyManager::update() {
