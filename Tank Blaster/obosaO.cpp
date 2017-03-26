f1d948d74e34f3441c6df467e0c4956bf3445351
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2017-03-26 16:16:53 -0700

    Add top bar logic comments to obosaO.cpp with FIXME

--- Tank Blaster/obosaO.cpp
+++ Tank Blaster/obosaO.cpp
@@ -1,18 +1,40 @@
 /**
  * Source File: obosaO.cpp
  * Description: Obosa Osagie-Amayo's code for Tank Blaster
+ * SECTIONS:    1. Top Bar - upper most bar for heads up display
+ *              2. Box Behavior - give tanks a box-like movement behavior
  **/
 
-//#include "./entity/unit/Behavior.cpp"
 #include <stdlib.h>
 
 #define WINDOWS_HEIGHT 800
 #define WINDOWS_WIDTH 600
 
+struct HUD: public Shape {
+  Shape height, width;
+  Vec color, location;
+};
+
 struct BoxBehavior : public Behavior {
   void behave (Node &model);
   BoxBehavior();
 };
+
+// =============================================================================
+// SECTION::Top Bar / FIXME -- incorrect object instantiation
+// -----------------------------------------------------------------------------
+/*HUD topBar;
+topBar.width    = WINDOWS_WIDTH;
+topBar.height   = WINDOWS_HEIGHT/10;
+topBar.color    = blue;
+topBar.location = Vec(WINDOWS_WIDTH/2, WINDOWS_HEIGHT-topBar.height, 0);
+attachChild(topBar);*/
+
+// =============================================================================
+
+// =============================================================================
+// SECTION::Box Behavior
+// -----------------------------------------------------------------------------
 
 void BoxBehavior::behave (Node &model) {
   Tank &tank = (Tank&) model;
@@ -26,7 +48,7 @@ void BoxBehavior::behave (Node &model) {
 		tank.moveUpRight();
 		box = 2;
 	}
-	  
+
    if (tank.location.x <= WINDOWS_WIDTH) {
 	    tank.moveUpRight();
 		}
@@ -35,7 +57,7 @@ void BoxBehavior::behave (Node &model) {
 		tank.moveLeft();
 	}
 
-	if (tank.location.x <= (WINDOWS_WIDTH - 20) && tank.location.y <= 
+	if (tank.location.x <= (WINDOWS_WIDTH - 20) && tank.location.y <=
 		(WINDOWS_HEIGHT/2)) {
 		tank.moveUp();
 	}
@@ -43,6 +65,8 @@ void BoxBehavior::behave (Node &model) {
 		(WINDOWS_HEIGHT/2)) {
 		tank.moveDown();
 	}
- 
+
   }
 }
+
+// =============================================================================
