520e25b7f6427f5ead4450f997ce647bf579e38d
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2017-04-26 08:40:23 -0700

    change tank movement speed

--- obosaO.h
+++ obosaO.h
@@ -14,7 +14,7 @@ Player::Player() : tank(Vec(WINDOW_WIDTH/2,WINDOW_HEIGHT/2,0)) {
 }
 
 void Player::initTank() {
-	tank.moveSpeed = .05;
+	tank.moveSpeed = 1;
 }
 
 //Manages Player Actions and Behavior
